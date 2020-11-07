f66bf79ab9a6826bc6bb80dc61cb1b51b0ade4e1
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-11-07 18:38:04 +0530

    Lets get started

---
+++ src/pages/Main.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+const Main = () => {
+  return (
+    <div>
+      <Navbar />
+      <h1>Here goes the IDE</h1>
+    </div>
+  );
+};
+
+export default Main;
