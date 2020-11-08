f656eb130659d0304d17bdd5f3894161a9a262e2
bhavleen-singh-au7 <singhbhavleen3@gmail.com>
2020-11-08 14:31:48 +0530

    cross origin resource sharing cofig added

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const request = require("request");
+const cors = require("cors");
 require("dotenv").config();
 
 const app = express();
 
 app.use(express.json({ extended: false }));
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hit POST request from POSTMAN.");
@@ -31,18 +33,17 @@ app.post("/", (req, res) => {
         json: reqBody,
       },
       (err, data) => {
-        if(err){
+        if (err) {
           res.status(400).json({
             error: err.message,
           });
         }
-        
+
         res.status(200).json({
           output: data.body,
         });
       }
     );
-
   } catch (err) {
     res.status(400).json({
       error: err.message,
@@ -50,7 +51,7 @@ app.post("/", (req, res) => {
   }
 });
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 5500;
 app.listen(PORT, () => {
   console.log("Listening on Port No.", PORT);
 });
